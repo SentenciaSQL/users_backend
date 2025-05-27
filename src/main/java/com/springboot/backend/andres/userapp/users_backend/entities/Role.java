f3430730c4fb6a44e9a538a57cd6018c1a4d27ea
Andrés Frías <inf512.andres.frias@gmail.com>
2025-05-26 22:14:54 -0400

    aws set

--- src/main/java/com/springboot/backend/andres/userapp/users_backend/entities/Role.java
+++ src/main/java/com/springboot/backend/andres/userapp/users_backend/entities/Role.java
@@ -14,6 +14,6 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 20)
+    @Column(length = 20, unique = true)
     private String name;
 }
